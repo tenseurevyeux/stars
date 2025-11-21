0bb33501954b7d2cbed050fc6cf2460dd9d04c70
tenseurevyeux <noahdd.s@ya.ru>
2025-11-21 18:25:48 +0500

    TLS HandShake added

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,43 @@
+use std::io::Cursor;
+use std::sync::Arc;
+use std::error::Error;
 use tokio::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use std::error::Error;
+use rustls_pki_types::CertificateDer;
+use rustls::ClientConfig;
+use tokio_rustls::TlsConnector;
+use rustls::pki_types::ServerName;
 
 
-#[tokio::main] 
+const CERT_BYTES: &[u8] = include_bytes!("../../certificates/rootCA.pem"); 
+
+
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut socket  = TcpStream::connect("10.38.75.248:7777").await?;
+    let mut root_store = rustls::RootCertStore::empty();
+    
+    let mut reader = Cursor::new(CERT_BYTES);
+    let certs: Vec<CertificateDer> = rustls_pemfile::certs(&mut reader).collect::<Result<Vec<_>, _>>()?;
+    for cert in certs {
+        root_store.add(cert)?;
+    }
 
-    socket.write_all(b"Hello?").await?;
+    let config = ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+    
+    let connector = TlsConnector::from(Arc::new(config));
+
+    let stream = TcpStream::connect("10.38.75.248:7777").await?;
+
+    let domain = ServerName::try_from("10.38.75.248")?; 
+    let mut socket = connector.connect(domain, stream).await?;
+
+    socket.write_all(b"Hello Secure World!").await?;
 
     let mut buf = [0u8; 1024];
     let n = socket.read(&mut buf).await?;
-
-    let response = String::from_utf8_lossy(&buf[..n]);
-    println!("response: {}", response);
+    println!("Response: {}", String::from_utf8_lossy(&buf[..n]));
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,34 +1,69 @@
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use std::fs::File;
+use std::io::BufReader;
+use std::sync::Arc;
 use std::error::Error;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_rustls::TlsAcceptor;
+use rustls::server::ServerConfig;
+use rustls_pki_types::{CertificateDer, PrivateKeyDer};
 
-async fn process(mut socket: TcpStream) -> Result<(), Box<dyn Error>> {
-    let mut buf= [0u8; 1024];
+
+fn load_tls_config() -> Result<ServerConfig, Box<dyn Error>> {
+    let cert_file = File::open("server.crt")?;
+    let mut cert_reader = BufReader::new(cert_file);
+    
+    let certs: Vec<CertificateDer> = rustls_pemfile::certs(&mut cert_reader)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let key_file = File::open("server.key")?;
+    let mut key_reader = BufReader::new(key_file);
+
+    let key: PrivateKeyDer = rustls_pemfile::private_key(&mut key_reader)?
+        .ok_or("No private key found in key.pem")?;
+
+    let config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)?;
+
+    Ok(config)
+}
+
+async fn process(mut socket: tokio_rustls::server::TlsStream<TcpStream>) -> Result<(), Box<dyn Error>> {
+    let mut buf = [0u8; 1024];
     let n = socket.read(&mut buf).await?;
 
-    if n == 0 {
-        return Ok(());
-    }
+    if n == 0 { return Ok(()); }
 
-    let recieve_data = String::from_utf8_lossy(&buf[..n]);
-    println!("msg: {}", recieve_data);
+    let msg = String::from_utf8_lossy(&buf[..n]);
+    println!("Recived {}", msg);
 
-    socket.write_all(b"successful conn").await?;
-
+    socket.write_all(b"Secure Hello!").await?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("10.38.75.248:8080").await?;
-    println!("Server started");
-    loop { 
-        let (socket, addr) = listener.accept().await?;
-        println!("New user connected: {}", addr);
-        
+    let config = load_tls_config()?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
+    let listener = TcpListener::bind("10.38.75.248:7777").await?;
+    println!("Secure Server started...");
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+
+        println!("New connection: {}", addr);
+
         tokio::spawn(async move {
-            if let Err(e) = process(socket).await {
-                eprintln!("Error \n {}", e); 
+            match acceptor.accept(stream).await {
+                Ok(tls_stream) => {
+                    if let Err(e) = process(tls_stream).await {
+                        eprintln!("Error processing: {}", e);
+                    }
+                }
+                Err(e) => eprintln!("TLS Handshake error: {}", e),
             }
         });
     }
